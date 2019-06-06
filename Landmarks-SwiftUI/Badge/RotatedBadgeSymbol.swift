598a2d508ab9f7d8d82a9adf0a3876f5f712a2c4
Alex Cooper <cooparr@hotmail.co.uk>
2019-06-06 16:57:12 +0100

    Finished Drawing Paths & Shapes

---
+++ Landmarks-SwiftUI/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Alex Cooper on 06/06/2019.
+//  Copyright © 2019 Alexander Cooper. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 25))
+    }
+}
