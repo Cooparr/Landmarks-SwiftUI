e0792d78f16c827d391e40359430a6b22f543f4d
Alex Cooper <cooparr@hotmail.co.uk>
2019-06-07 15:58:59 +0100

    Composing Complex Interfaces project setup

---
+++ Landmarks-SwiftUI/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Alex Cooper on 07/06/2019.
+//  Copyright © 2019 Alexander Cooper. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome : View {
+    var body: some View {
+        Text("Landmarks Home")
+    }
+}
+
+#if DEBUG
+struct Home_Previews : PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
+#endif
