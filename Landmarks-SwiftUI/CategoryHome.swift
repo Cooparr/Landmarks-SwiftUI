0f35fc3edebdab6a198edca16e83556d5e2cfd6f
Alex Cooper <cooparr@hotmail.co.uk>
2019-06-07 16:26:02 +0100

    Section 3

--- Landmarks-SwiftUI/CategoryHome.swift
+++ Landmarks-SwiftUI/CategoryHome.swift
@@ -9,8 +9,25 @@
 import SwiftUI
 
 struct CategoryHome : View {
+    
+    var categories: [String: [Landmark]] {
+        .init(
+            grouping: landmarkData,
+            by: {
+                $0.category.rawValue
+        })
+    }
+    
+    
     var body: some View {
-        Text("Landmarks Home")
+        NavigationView {
+            List {
+                ForEach(categories.keys.sorted().identified(by: \.self)) { key in
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
+        }
     }
 }
 
---
+++ Landmarks-SwiftUI/CategoryRow.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryRow.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Alex Cooper on 07/06/2019.
+//  Copyright © 2019 Alexander Cooper. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow : View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+            .font(.headline)
+            .padding(.leading, 15)
+            .padding(.top, 5)
+            
+            ScrollView(showsHorizontalIndicator: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#if DEBUG
+struct CategoryRow_Previews : PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+    }
+}
+#endif
