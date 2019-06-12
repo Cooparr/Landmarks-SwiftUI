001f6248e5950f28d7fc047e15a86747fcde1eaa
Alex Cooper <cooparr@hotmail.co.uk>
2019-06-12 12:26:35 +0100

    Section 3

--- Landmarks-SwiftUI/PageViewController.swift
+++ Landmarks-SwiftUI/PageViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 struct PageViewController : UIViewControllerRepresentable {
     var controllers: [UIViewController]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -19,14 +20,15 @@ struct PageViewController : UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController( transitionStyle: .scroll, navigationOrientation: .horizontal)
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([controllers[0]], direction: .forward, animated: true)
+        pageViewController.setViewControllers([controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         
         init(_ pageViewController: PageViewController) {
@@ -58,6 +60,14 @@ struct PageViewController : UIViewControllerRepresentable {
             }
             return parent.controllers[index + 1]
         }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+                let visibleViewController = pageViewController.viewControllers?.first,
+                let index = parent.controllers.firstIndex(of: visibleViewController)
+            {
+                parent.currentPage = index
+            }
+        }
     }
 }
-
